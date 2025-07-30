d8e6c38c695c8003ced56b68063e3f8f7fef3b7b
sabhyasachisahoo <sabhyasachisahoo16@gmail.com>
2025-07-30 18:14:13 +0530

    Added Redis service file

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -3,9 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const redisClient = new Redis(process.env.REDIS_URL, {
-  tls: {}  // ⚠️ Required for Redis Cloud over SSL
-}); // no TLS
+const redisClient = new Redis(process.env.REDIS_URL); // no TLS
 
 redisClient.on('connect', () => {
   console.log('✅ Redis connected successfully');

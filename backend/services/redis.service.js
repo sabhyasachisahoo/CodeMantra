e82e2655b6389b3fe3c890a88d237aeb3934c78d
sabhyasachisahoo <sabhyasachisahoo16@gmail.com>
2025-07-30 20:12:35 +0530

    Add Redis service connection file

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,11 +1,30 @@
+// import Redis from 'ioredis';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const redisClient = new Redis(process.env.REDIS_URL,{
+//   tls: {}
+// }); // no TLS
+
+// redisClient.on('connect', () => {
+//   console.log('✅ Redis connected successfully');
+// });
+
+// redisClient.on('error', (err) => {
+//   console.error('❌ Redis connection error:', err);
+// });
+
+// export default redisClient;
+
 import Redis from 'ioredis';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
-const redisClient = new Redis(process.env.REDIS_URL,{
-  tls: {}
-}); // no TLS
+const redisClient = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+});
 
 redisClient.on('connect', () => {
   console.log('✅ Redis connected successfully');
